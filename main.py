0098ad707b4fb7a7f86623d6eb7908cf3cc7bb4a
OlamoyegunAyomide <olamoyegunayomide16@gmail.com>
2024-03-23 15:40:38 +0100

    corrected CORS

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 #application setup phase
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    CORS(app, origins = ["http://localhost:3000", "https://localhost:3000"])
+    CORS(app, origins = ["*"])
 
     app.config.from_mapping(
         SECRET_KEY='dev',
